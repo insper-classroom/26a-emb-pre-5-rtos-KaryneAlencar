cf5568e8844bcd39248a47cd66cb01daf71ed98e
karyne <karyneam@al.insper.edu.br>
2026-04-09 09:22:48 -0300

    3

--- exe3/main.c
+++ exe3/main.c
@@ -12,8 +12,24 @@ const int BTN_PIN_G = 26;
 const int LED_PIN_R = 4;
 const int LED_PIN_G = 6;
 
-QueueHandle_t xQueueButId;
-QueueHandle_t xQueueButId2;
+QueueHandle_t xQueueLedR;
+QueueHandle_t xQueueLedG;
+QueueHandle_t xQueueBtn;
+
+void btn_callback(uint gpio, uint32_t events) {
+  int btn = 0;
+  if(events == 0x4){
+    if(gpio == BTN_PIN_G){
+      btn = 2;
+    }
+    if(gpio == BTN_PIN_R){
+      btn = 1;
+    }
+  }
+  if(btn != 0){
+    xQueueSendFromISR(xQueueBtn, &btn, 0);
+  }
+}
 
 void led_1_task(void *p) {
     gpio_init(LED_PIN_R);
@@ -21,7 +37,7 @@ void led_1_task(void *p) {
 
     int delay = 0;
     while (true) {
-        if (xQueueReceive(xQueueButId, &delay, 0)) {
+        if (xQueueReceive(xQueueLedR, &delay, 0)) {
             printf("%d\n", delay);
         }
 
@@ -34,37 +50,13 @@ void led_1_task(void *p) {
     }
 }
 
-void btn_1_task(void *p) {
-    gpio_init(BTN_PIN_R);
-    gpio_set_dir(BTN_PIN_R, GPIO_IN);
-    gpio_pull_up(BTN_PIN_R);
-
-    int delay = 0;
-    while (true) {
-        if (!gpio_get(BTN_PIN_R)) {
-
-            while (!gpio_get(BTN_PIN_R)) {
-                vTaskDelay(pdMS_TO_TICKS(1));
-            }
-
-            if (delay < 1000) {
-                delay += 100;
-            } else {
-                delay = 100;
-            }
-            printf("delay btn %d \n", delay);
-            xQueueSend(xQueueButId, &delay, 0);
-        }
-    }
-}
-
 void led_2_task(void *p) {
     gpio_init(LED_PIN_G);
     gpio_set_dir(LED_PIN_G, GPIO_OUT);
 
     int delay = 0;
     while (true) {
-        if (xQueueReceive(xQueueButId2, &delay, 0)) {
+        if (xQueueReceive(xQueueLedG, &delay, 0)) {
             printf("%d\n", delay);
         }
 
@@ -77,26 +69,39 @@ void led_2_task(void *p) {
     }
 }
 
-void btn_2_task(void *p) {
+void btn_task(void *p) {
     gpio_init(BTN_PIN_G);
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
+    gpio_init(BTN_PIN_R);
+    gpio_set_dir(BTN_PIN_R, GPIO_IN);
+    gpio_pull_up(BTN_PIN_R);
 
     int delay = 0;
+    int btn;
     while (true) {
-        if (!gpio_get(BTN_PIN_G)) {
+        if (xQueueReceive( xQueueBtn, &btn, pdMS_TO_TICKS(100))) {
 
-            while (!gpio_get(BTN_PIN_G)) {
-                vTaskDelay(pdMS_TO_TICKS(1));
+            if(btn == 2){
+                if (delay < 1000) {
+                    delay += 100;
+                } else {
+                    delay = 100;
+                }
+                printf("delay btn %d \n", delay);
+                xQueueSend(xQueueLedG, &delay, 0);
             }
 
-            if (delay < 1000) {
-                delay += 100;
-            } else {
-                delay = 100;
+            if(btn == 1){
+                if (delay < 1000) {
+                    delay += 100;
+                } else {
+                    delay = 100;
+                }
+                printf("delay btn %d \n", delay);
+                xQueueSend(xQueueLedR, &delay, 0);
             }
-            printf("delay btn %d \n", delay);
-            xQueueSend(xQueueButId2, &delay, 0);
+            
         }
     }
 }
@@ -105,13 +110,16 @@ int main() {
     stdio_init_all();
     printf("Start RTOS \n");
 
-    xQueueButId = xQueueCreate(32, sizeof(int));
-    xQueueButId2 = xQueueCreate(32, sizeof(int));
+    xQueueBtn = xQueueCreate(32, sizeof(int));
+    xQueueLedG = xQueueCreate(32, sizeof(int));
+    xQueueLedR = xQueueCreate(32, sizeof(int));
+
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+    gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
     xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
-    xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 1, NULL);
     xTaskCreate(led_2_task, "LED2_Task 1", 256, NULL, 1, NULL);
-    xTaskCreate(btn_2_task, "BTN2_Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(btn_task, "BTN_Task 1", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
